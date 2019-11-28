6734d18fd4a53b656bfe34a71543db6ee1392154
Mara Houbraken <mara.houbraken@philips.com>
2019-11-28 19:17:20 +0100

    Adds code to check if a bomb hit a ship

--- tests/zeeslagje_test.py
+++ tests/zeeslagje_test.py
@@ -20,3 +20,30 @@ def test_board_is_square_and_size(size):
     board = zeeslagje.Board(size)
     assert len(board.board) == size
     assert all(len(row) == size for row in board.board)
+
+
+def test_when_board_is_initialized_as_zeroes_bomb_returns_zero():
+    board = zeeslagje.Board()
+    row_count = 0
+    for row in board.board:
+        col_count = 0
+        for col in row:
+            assert board.bomb(row=row_count, col=col_count) is 0
+            col_count += 1
+        row_count += 1
+
+
+def test_ship_is_recognised_on_board():
+    bomb = False
+    board = zeeslagje.Board()
+    board.board[0][0] = 1
+    row_count = 0
+    for row in board.board:
+        col_count = 0
+        for col in row:
+            if board.bomb(row=row_count, col=col_count) != 0:
+                bomb = True
+            col_count += 1
+        row_count += 1
+
+    assert bomb is True
--- zeeslagje/zeeslagje.py
+++ zeeslagje/zeeslagje.py
@@ -4,3 +4,7 @@ from typing import List
 class Board:
     def __init__(self, size: int = 10):
         self.board: List[List[int]] = [[0] * size] * size
+
+    def bomb(self, row, col):
+        return self.board[row][col]
+
